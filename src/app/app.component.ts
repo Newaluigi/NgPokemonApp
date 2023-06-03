180ba744ceb121ce994a584fdcfd2fe9beaa1532
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-03 23:47:26 +0200

    templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,31 @@ import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Liste de Pokémons</h1>
-
-    <!-- <router-outlet></router-outlet> --> `,
+  templateUrl: "./app.component.html",
 })
+// <router-outlet></router-outlet>
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
+    // this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    // const index: number = +(event.target as HTMLInputElement).value;
+    // const id = +pokemonId;
+
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id === +pokemonId
+    );
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      alert(`Vous avez demandé un pokemon non répertorié`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }

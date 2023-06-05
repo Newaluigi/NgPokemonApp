4acaf0954c757a21f87523c54ea62880893ac1e9
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-05 02:25:55 +0200

    pipes

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -4,31 +4,30 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
   selector: "[pkmnBorderCard]",
 })
 export class BorderCardDirective {
-  // private initialColor: string; = '#f5f5f5';
-  // private defaultColor: string; = '#009688';
-  // private defaultHeight
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 180;
 
   constructor(private el: ElementRef) {
-    this.setHeight(180);
-    this.setBorder("#f5f5f5");
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
 
   @Input("pkmnBorderCard") borderColor: string; //alias
-  @Input("pkmnBorderCard") borderHeight: number;
 
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder(this.borderColor || "#009688");
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder(this.borderColor || "#f5f5f5");
+    this.setBorder(this.initialColor);
   }
 
   setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
 
-  setBorder(color: string) {
+  private setBorder(color: string) {
     let border = "solid 4px " + color;
     this.el.nativeElement.style.border = border;
   }

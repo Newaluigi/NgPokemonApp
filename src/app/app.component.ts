79156e680947e5870cd073b7b73f29bc0dd99dcb
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-07 02:15:26 +0200

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
+import { Component } from "@angular/core";
+// import { POKEMONS } from "./mock-pokemon-list";
+// import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
 // <router-outlet></router-outlet>
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-    // this.selectPokemon(this.pokemonList[0]);
-  }
-
-  selectPokemon(pokemonId: string) {
-    // const index: number = +(event.target as HTMLInputElement).value;
-    // const id = +pokemonId;
-
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id === +pokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokemon non répertorié`);
-      this.pokemonSelected = pokemon;
-    }
-  }
+export class AppComponent {
+  // pokemonList: Pokemon[] = POKEMONS;
+  // pokemonSelected: Pokemon | undefined;
+  // ngOnInit() {
+  //   console.table(this.pokemonList);
+  //   // this.selectPokemon(this.pokemonList[0]);
+  // }
+  // selectPokemon(pokemonId: string) {
+  //   // const index: number = +(event.target as HTMLInputElement).value;
+  //   // const id = +pokemonId;
+  //   const pokemon: Pokemon | undefined = this.pokemonList.find(
+  //     (pokemon) => pokemon.id === +pokemonId
+  //   );
+  //   if (pokemon) {
+  //     console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+  //     this.pokemonSelected = pokemon;
+  //   } else {
+  //     console.log(`Vous avez demandé un pokemon non répertorié`);
+  //     this.pokemonSelected = pokemon;
+  //   }
+  // }
 }

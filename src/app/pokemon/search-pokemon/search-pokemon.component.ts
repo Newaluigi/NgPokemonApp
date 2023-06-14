21600d4c77f8cb002a1cd3d4093bcd13bae51551
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-14 23:19:48 +0200

    RxJS

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-search-pokemon',
-  templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
+  selector: "app-search-pokemon",
+  templateUrl: "./search-pokemon.component.html",
+  styles: [],
 })
 export class SearchPokemonComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  searchPokemon(term: string) {}
+
+  goToDetail(pokemon: Pokemon) {
+    ù;
+    const link = ["pokemon", pokemon.id];
+    this.router.navigate(link);
+  }
 }
